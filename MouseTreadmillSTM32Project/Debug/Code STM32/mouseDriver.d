dae61abccf73d420b576b945259b3c8f92655e3f
Didier Negretto <didiernegretto96@gmail.com>
2019-10-28 20:06:59 +0100

    STM32 project created

---
+++ MouseTreadmillSTM32Project/Debug/Code STM32/mouseDriver.d
@@ -0,0 +1,139 @@
+Code STM32/mouseDriver.o: \
+ /Users/Didier/Desktop/EPFL/Secondo\ master/SemesterProject2019/GITRepository/3DMouseTreadmill/Code\ STM32/mouseDriver.c \
+ /Users/Didier/Desktop/EPFL/Secondo\ master/SemesterProject2019/GITRepository/3DMouseTreadmill/Code\ STM32/mouseDriver.h \
+ /Users/Didier/Desktop/EPFL/Secondo\ master/SemesterProject2019/GITRepository/3DMouseTreadmill/MAVLink\ Library/mouse/mavlink.h \
+ /Users/Didier/Desktop/EPFL/Secondo\ master/SemesterProject2019/GITRepository/3DMouseTreadmill/MAVLink\ Library/mouse/version.h \
+ /Users/Didier/Desktop/EPFL/Secondo\ master/SemesterProject2019/GITRepository/3DMouseTreadmill/MAVLink\ Library/mouse/mouse.h \
+ /Users/Didier/Desktop/EPFL/Secondo\ master/SemesterProject2019/GITRepository/3DMouseTreadmill/MAVLink\ Library/mouse/../protocol.h \
+ /Users/Didier/Desktop/EPFL/Secondo\ master/SemesterProject2019/GITRepository/3DMouseTreadmill/MAVLink\ Library/mouse/../mavlink_types.h \
+ /Users/Didier/Desktop/EPFL/Secondo\ master/SemesterProject2019/GITRepository/3DMouseTreadmill/MAVLink\ Library/mouse/../mavlink_helpers.h \
+ /Users/Didier/Desktop/EPFL/Secondo\ master/SemesterProject2019/GITRepository/3DMouseTreadmill/MAVLink\ Library/mouse/../checksum.h \
+ /Users/Didier/Desktop/EPFL/Secondo\ master/SemesterProject2019/GITRepository/3DMouseTreadmill/MAVLink\ Library/mouse/../mavlink_conversions.h \
+ /Users/Didier/Desktop/EPFL/Secondo\ master/SemesterProject2019/GITRepository/3DMouseTreadmill/MAVLink\ Library/mouse/../mavlink_sha256.h \
+ /Users/Didier/Desktop/EPFL/Secondo\ master/SemesterProject2019/GITRepository/3DMouseTreadmill/MAVLink\ Library/mouse/./mavlink_msg_heartbeat.h \
+ /Users/Didier/Desktop/EPFL/Secondo\ master/SemesterProject2019/GITRepository/3DMouseTreadmill/MAVLink\ Library/mouse/./mavlink_msg_speed_info.h \
+ /Users/Didier/Desktop/EPFL/Secondo\ master/SemesterProject2019/GITRepository/3DMouseTreadmill/MAVLink\ Library/mouse/./mavlink_msg_speed_setpoint.h \
+ /Users/Didier/Desktop/EPFL/Secondo\ master/SemesterProject2019/GITRepository/3DMouseTreadmill/MAVLink\ Library/mouse/./mavlink_msg_mode_selection.h \
+ /Users/Didier/Desktop/EPFL/Secondo\ master/SemesterProject2019/GITRepository/3DMouseTreadmill/MAVLink\ Library/mouse/../mavlink_get_info.h \
+ ../Core/Inc/main.h ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h \
+ ../Core/Inc/stm32l4xx_hal_conf.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_def.h \
+ ../Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h \
+ ../Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l476xx.h \
+ ../Drivers/CMSIS/Include/core_cm4.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Include/mpu_armv7.h \
+ ../Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_exti.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio_ex.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma_ex.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_cortex.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ex.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ramfunc.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c_ex.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart_ex.h
+
+/Users/Didier/Desktop/EPFL/Secondo\ master/SemesterProject2019/GITRepository/3DMouseTreadmill/Code\ STM32/mouseDriver.h:
+
+/Users/Didier/Desktop/EPFL/Secondo\ master/SemesterProject2019/GITRepository/3DMouseTreadmill/MAVLink\ Library/mouse/mavlink.h:
+
+/Users/Didier/Desktop/EPFL/Secondo\ master/SemesterProject2019/GITRepository/3DMouseTreadmill/MAVLink\ Library/mouse/version.h:
+
+/Users/Didier/Desktop/EPFL/Secondo\ master/SemesterProject2019/GITRepository/3DMouseTreadmill/MAVLink\ Library/mouse/mouse.h:
+
+/Users/Didier/Desktop/EPFL/Secondo\ master/SemesterProject2019/GITRepository/3DMouseTreadmill/MAVLink\ Library/mouse/../protocol.h:
+
+/Users/Didier/Desktop/EPFL/Secondo\ master/SemesterProject2019/GITRepository/3DMouseTreadmill/MAVLink\ Library/mouse/../mavlink_types.h:
+
+/Users/Didier/Desktop/EPFL/Secondo\ master/SemesterProject2019/GITRepository/3DMouseTreadmill/MAVLink\ Library/mouse/../mavlink_helpers.h:
+
+/Users/Didier/Desktop/EPFL/Secondo\ master/SemesterProject2019/GITRepository/3DMouseTreadmill/MAVLink\ Library/mouse/../checksum.h:
+
+/Users/Didier/Desktop/EPFL/Secondo\ master/SemesterProject2019/GITRepository/3DMouseTreadmill/MAVLink\ Library/mouse/../mavlink_conversions.h:
+
+/Users/Didier/Desktop/EPFL/Secondo\ master/SemesterProject2019/GITRepository/3DMouseTreadmill/MAVLink\ Library/mouse/../mavlink_sha256.h:
+
+/Users/Didier/Desktop/EPFL/Secondo\ master/SemesterProject2019/GITRepository/3DMouseTreadmill/MAVLink\ Library/mouse/./mavlink_msg_heartbeat.h:
+
+/Users/Didier/Desktop/EPFL/Secondo\ master/SemesterProject2019/GITRepository/3DMouseTreadmill/MAVLink\ Library/mouse/./mavlink_msg_speed_info.h:
+
+/Users/Didier/Desktop/EPFL/Secondo\ master/SemesterProject2019/GITRepository/3DMouseTreadmill/MAVLink\ Library/mouse/./mavlink_msg_speed_setpoint.h:
+
+/Users/Didier/Desktop/EPFL/Secondo\ master/SemesterProject2019/GITRepository/3DMouseTreadmill/MAVLink\ Library/mouse/./mavlink_msg_mode_selection.h:
+
+/Users/Didier/Desktop/EPFL/Secondo\ master/SemesterProject2019/GITRepository/3DMouseTreadmill/MAVLink\ Library/mouse/../mavlink_get_info.h:
+
+../Core/Inc/main.h:
+
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h:
+
+../Core/Inc/stm32l4xx_hal_conf.h:
+
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc.h:
+
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_def.h:
+
+../Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h:
+
+../Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l476xx.h:
+
+../Drivers/CMSIS/Include/core_cm4.h:
+
+../Drivers/CMSIS/Include/cmsis_version.h:
+
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+
+../Drivers/CMSIS/Include/mpu_armv7.h:
+
+../Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h:
+
+../Drivers/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h:
+
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h:
+
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_exti.h:
+
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio.h:
+
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio_ex.h:
+
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma.h:
+
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma_ex.h:
+
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_cortex.h:
+
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash.h:
+
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ex.h:
+
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ramfunc.h:
+
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c.h:
+
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c_ex.h:
+
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr.h:
+
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h:
+
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart.h:
+
+../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart_ex.h:
