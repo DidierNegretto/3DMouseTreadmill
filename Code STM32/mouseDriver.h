051751c8697748c68e6594add22993483f4ee46e
Didier Negretto <didiernegretto96@gmail.com>
2019-10-28 12:48:38 +0100

    Folder for mouse driver created

---
+++ Code STM32/mouseDriver.h
@@ -0,0 +1,8 @@
+/* ***********************************************************
+ * Mouse driver header.
+ * Author : Didier Negretto
+ * 
+ * This file contains all the functions required in the stm32 for 
+ * control, packing and unpacking of the messages.
+ * ***********************************************************/ 
+
