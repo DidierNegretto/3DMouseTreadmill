5f102e873ced4b0fc534b19ad068719ca31723b3
Didier Negretto <didiernegretto96@gmail.com>
2020-01-06 20:06:30 +0100

    Parsing of log data done successfully

--- MouseTreadmillPC/python/plot.py
+++ MouseTreadmillPC/python/plot.py
@@ -1,12 +1,12 @@
 import os
 import sys
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
 import json
 
 files = ["log_0.01_speed_y.txt","log_0.02_speed_y.txt","log_0.03_speed_y.txt","log_0.04_speed_y.txt"]
-start_time = []
-stop_time = []
+start_time = [2063500, 2173590, 2275030, 15595]
+stop_time =  [2123700, 2238690, 2354990, 74673]
 log = []
 
 for i in range(4):
@@ -36,9 +36,32 @@ for i in range(4):
                 log[i][data[0]][stuff[j][0]] = []
             log[i][data[0]][stuff[j][0]].append(float(stuff[j][1]))
 
-    
+fig = plt.figure()
+ax = []
+ax.append(fig.add_subplot(221))
+ax.append(fig.add_subplot(222))
+ax.append(fig.add_subplot(223))
+ax.append(fig.add_subplot(224))
 
-print(log[1]["RAW_SENSOR"]['time'])
+for i in range(4):
+    ax[i].plot(log[i]["SPEED_INFO"]["time_y"], log[i]["SPEED_INFO"]["speed_y"])
+
+fig = plt.figure()
+ax = []
+ax.append(fig.add_subplot(221))
+ax.append(fig.add_subplot(222))
+ax.append(fig.add_subplot(223))
+ax.append(fig.add_subplot(224))
+
+for i in range(4):
+    t = [el for el in log[i]["SPEED_INFO"]["time_y"] if (el>start_time[i] and el<stop_time[i])]
+    start_index = log[i]["SPEED_INFO"]["time_y"].index(t[0])
+    end_index = log[i]["SPEED_INFO"]["time_y"].index(t[-1])
+    y = log[i]["SPEED_INFO"]["speed_y"][start_index:end_index+1]
+    ax[i].plot(t, y)
+    
+plt.show()
+
 
 
 
